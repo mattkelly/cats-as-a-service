cace2a3c209fb81471875e7138835a898878dd6e
Jayson Messenger <jayson.messenger@gmail.com>
2019-10-22 16:46:59 -0400

    Head is now a group containing the ellipse, face and ears

--- head.js
+++ head.js
@@ -1,13 +1,24 @@
 'use strict';
 
-SVG.EllipseHead = SVG.invent({
-    create: 'ellipse',
+require('./face.js');
+require('./ear.js');
 
-    inherit: SVG.Ellipse,
+SVG.EllipseHead = SVG.invent({
+    create: function(width, height, colorscheme) {
+        SVG.G.call(this);
+
+        this.leftEar(colorscheme)
+        this.rightEar(colorscheme)
+        this.ellipse(width, height).fill(colorscheme.secondary)
+        this.face(width, 50, colorscheme).move(20, height/2 - 10);
+
+    },
+
+    inherit: SVG.G,
 
     construct: {
-        ellipseHead: function(width, height) {
-            return this.put(new SVG.EllipseHead).size(width, height).move(0, 0);
+        ellipseHead: function(width, height, colorscheme) {
+            return this.put(new SVG.EllipseHead(width, height, colorscheme).move(0, 0));
         }
     }
 });
